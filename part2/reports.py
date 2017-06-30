4fbfa2ef9dc5724414f3f7f787c921b437daed1d
Balint Mogyorosi <bg.mogyorosi@gmail.com>
2017-06-30 18:32:22 +0200

    get_most_played() finished

--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,24 @@
-
 # Report functions
+
+
+def import_games(file_name):
+    file = open(file_name, "r")
+    content = file.read().split('\n')
+    games = []
+    for line in content:
+        game = line.split('\t') 
+        if len(game) == 5:
+            game[1] = float(game[1])
+            game[2] = int(game[2])
+            games.append(game)
+    file.close()
+    return games
+
+
+def get_most_played(file_name):
+    games = import_games(file_name)
+    max_sold = 0
+    for i in range(len(games)):
+        if games[i][1] > games[max_sold][1]:
+            max_sold = i
+    return games[max_sold][0]
