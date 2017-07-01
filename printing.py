df3f7875d14407e229c80452773fcee237b32817
Balint Mogyorosi <bg.mogyorosi@gmail.com>
2017-07-01 12:49:41 +0200

    Printing.py finished, need to call export function too

--- printing.py
+++ printing.py
@@ -1,7 +1,7 @@
 # Printing functions
 
 
-import reports
+import reportsOOP
 
 
 def decor(func):
@@ -18,37 +18,46 @@ def print_exercise(number):
 
 
 file_name = 'game_stat.txt'
+datas = {}
+reportsOOP.get_datas(datas)
+repo = reportsOOP.Report(file_name)
 
 # Exercise 1
 print_exercise(1)
-print(reports.count_games(file_name))
+print(repo.count_games())
 
 # Exercise 2
 print_exercise(2)
-print(reports.decide(file_name, 2000))
+print(repo.decide(datas['year']))
 
 # Exercise 3
 print_exercise(3)
-print(reports.get_latest(file_name))
+print(repo.get_latest())
 
 # Exercise 4
 print_exercise(4)
-print(reports.count_by_genre(file_name, "First-person shooter"))
+print(repo.count_by_genre(datas['genre']))
 
 # Exercise 5
 print_exercise(5)
-print(reports.get_line_number_by_title(file_name, "Counter-Strike"))
+try:
+    print(repo.get_line_number_by_title(datas['title']))
+except ValueError:
+    print('There is no game called {}!'.format(datas['title']))
 
 # Exercise 6
 print_exercise(6)
-for title in reports.sort_abc(file_name):
+for title in repo.sort_abc():
     print(title)
 
 # Exercise 7
 print_exercise(7)
-for genre in reports.get_genres(file_name):
+for genre in repo.get_genres():
     print(genre)
 
 # Exercise 8
 print_exercise(8)
-print(reports.when_was_top_sold_fps(file_name))
+try:
+    print(repo.when_was_top_sold_fps())
+except ValueError:
+    print('There is not fps game in the stat!')
